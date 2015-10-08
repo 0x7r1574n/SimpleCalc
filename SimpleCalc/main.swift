30e5b458c225d64ac828d98020984f8bedacb43e
Cechi Shi <shicechi@gmail.com>
2015-10-08 16:03:13 -0700

    fully functional

--- SimpleCalc/main.swift
+++ SimpleCalc/main.swift
@@ -6,9 +6,6 @@
 //  Copyright © 2015 University of Washington. All rights reserved.
 //
 
-
-// Todo: check input string
-
 import Foundation
 
 func input() -> String {
@@ -18,13 +15,13 @@ func input() -> String {
     return result.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
 }
 
-func convert(incoming:String) -> Double {
-    return NSNumberFormatter().numberFromString(incoming)!.doubleValue
-    
+func convert(incoming:String) -> Int {
+    return NSNumberFormatter().numberFromString(incoming)!.integerValue
 }
 
-func singleOperation(firstPart:Double, secondPart:Double, operation:String) {
-    let result: Double?
+
+func singleOperation(firstPart:Int, secondPart:Int, operation:String) {
+    let result: Int?
     
     switch operation {
     case "+":
@@ -44,15 +41,61 @@ func singleOperation(firstPart:Double, secondPart:Double, operation:String) {
     if result != nil {
         print("Result: \(result!)")
     } else {
-        print("Invalid operation.")
+        print("Invalid opeartion.")
     }
 }
 
+func isNumber(incoming:String) -> Bool {
+    return Int(incoming) != nil || Double(incoming) != nil
+}
+
+func factorial(n:Int) -> Int {
+    return n == 0 ? 1 : n * factorial(n - 1)
+}
 
 
+// Prompt for user input
 print("Enter an expression separated by returns:")
 
-var firstPart = convert(input())
-var operation = input()
-var secondPart = convert(input())
-singleOperation(firstPart, secondPart: secondPart, operation: operation)
+// Current input
+var element: String
+var inventory = [String]()
+
+// Supported operations
+let operations = ["count", "fact", "avg", "+", "-", "*", "/", "%"]
+
+repeat {
+    element = input()
+    inventory.append(element)
+} while (isNumber(element))
+
+// If a valid or invalid opeartion is requested
+switch element {
+    case "count":
+        print("Result: \(inventory.count - 1)")
+    case "avg":
+        // Only if they have entered some numbers
+        if inventory.count > 1 {
+            var sum = convert(inventory[0])
+            for var i = 1; i < inventory.count - 1; i++ {
+                sum += convert(inventory[i])
+            }
+            print("Result: \(sum/(inventory.count - 1))")
+        } else {
+            print("Missing operands.")
+        }
+    case "fact":
+        // Has to be one number
+        if inventory.count == 2 {
+            print("Result: \(factorial(convert(inventory[0])))")
+        } else {
+            print("Too many numbers.")
+        }
+    case "+", "-", "*", "/", "%":
+        // Saves second part of the expression
+        var secondPart = input()
+        singleOperation(convert(inventory[0]), secondPart: convert(secondPart), operation: inventory[1])
+    default:
+        print("Invalid operation.")
+}
+
